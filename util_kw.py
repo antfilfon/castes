7289b0254ceeb39ef1258d0860d787c9e3651a2e
Antonio Filipe Fonseca <antoniofilipefonseca@gmail.com>
2018-04-07 10:26:15 +0530

    new version of util_kw

--- util_kw.py
+++ util_kw.py
@@ -1,8 +1,9 @@
 lower_caste_keywords = ['dalit', 'untouchable', 'sc_st', 'obc', 'lower_caste', 'minorities', 'backward_class']
-neutral_keywords = ['individual', 'man', 'woman', 'person', 'people', 'subject', 'object', 'human']
-upper_caste_keywords = ['upper_caste', 'kshatriya', 'brahmin', 'government', 'democracy', 'election', 'college', 'education', 'scholar', 'merit', 'meritorous', 'international', 'rich']
+upper_caste_keywords = ['upper_caste', 'brahmin', 'kshatriya', 'vaishya']
+priviledge_keywords = ['government', 'democracy', 'election', 'college', 'education', 'scholar', 'merit', 'meritorious', 'employer', 'national', 'international', 'rich']
 negative_aspect = ['poor', 'violence', 'disease', 'unhealthy', 'incident', 'crime']
 positive_aspect = ['empathy', 'care', 'companion', 'friend', 'healthy', 'prosperity']
+neutral_keywords = ['individual', 'man', 'woman', 'person', 'people', 'subject', 'object', 'human']
 
 def add_to_graph(word, assoclist, graph):
     graph.add_node(word)
@@ -10,7 +11,7 @@ def add_to_graph(word, assoclist, graph):
         kword, kweight = wtuple
         graph.add_node(kword)
         graph.add_edge(word, kword, weight=kweight)
-        
+
 def recurse_add_(word, wvmodel, graph, depth=1, topn=5):
     if depth==1:
         # call add_to_graph and return
@@ -23,3 +24,12 @@ def recurse_add_(word, wvmodel, graph, depth=1, topn=5):
         for wtuple in alist:
             aword, _ = wtuple
             recurse_add_(aword, wvmodel, graph, depth=depth-1, topn=topn)
+
+def word_add_(word, wvmodel, lista, depth=1, topn=5):
+    if depth==1:
+        lista[word] = wvmodel.most_similar(word, topn=topn)
+        return
+    else:
+        for wtuple in wvmodel.most_similar(word, topn=topn):
+            aword, _ = wtuple
+            word_add_(aword, wvmodel, lista, depth=depth-1, topn=topn)
